0bed01e8e063f024f46a675b3160cf8c54143da1
LiLanqi <1042520531@qq.com>
2022-10-23 01:34:19 +0800

    使用redis，增加了点赞功能

--- src/main/java/com/example/mycommunity/controller/HomeController.java
+++ src/main/java/com/example/mycommunity/controller/HomeController.java
@@ -4,7 +4,9 @@ import com.example.mycommunity.entity.DiscussPost;
 import com.example.mycommunity.entity.Page;
 import com.example.mycommunity.entity.User;
 import com.example.mycommunity.service.DiscussPostService;
+import com.example.mycommunity.service.LikeService;
 import com.example.mycommunity.service.UserService;
+import com.example.mycommunity.util.CommunityConstant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,13 +19,16 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
-public class HomeController {
+public class HomeController implements CommunityConstant {
 
     @Autowired
     private DiscussPostService discussPostService;
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private LikeService likeService;
 
     @RequestMapping(path = "/index", method = RequestMethod.GET)
     public String getIndexPage(Model model, Page page) {
@@ -41,6 +46,8 @@ public class HomeController {
                 User user = userService.findUserById(post.getUserId());
                 map.put("user", user);
                 discussPosts.add(map);
+                long likeCount = likeService.findEntityLikeCount(ENTITY_TYPE_POST, post.getId());
+                map.put("likeCount", likeCount);
             }
         }
         model.addAttribute("discussPosts", discussPosts);
