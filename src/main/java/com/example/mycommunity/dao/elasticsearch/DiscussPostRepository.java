c95d46158cc336223b88b7b19f6f1c50d664bfae
LiLanqi <1042520531@qq.com>
2023-02-11 16:04:33 +0800

    el相关应用

---
+++ src/main/java/com/example/mycommunity/dao/elasticsearch/DiscussPostRepository.java
@@ -0,0 +1,10 @@
+package com.example.mycommunity.dao.elasticsearch;
+
+import com.example.mycommunity.entity.DiscussPost;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DiscussPostRepository extends ElasticsearchRepository<DiscussPost, Integer> {
+
+}
